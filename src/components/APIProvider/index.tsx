9165cb53196f8eaafb4ef83093455e5cbb98d21c
Zaiwen Gong <zgong@zaiwens-mbp.lan>
2020-07-31 14:43:22 -0400

    Added abstraction layer

--- src/components/APIProvider/index.tsx
+++ src/components/APIProvider/index.tsx
@@ -1,10 +1,12 @@
 
-import React, { useState, createContext, ReactNode } from 'react';
+import React, { useEffect, useState, createContext, ReactNode } from 'react';
 import { RemoteParticipant } from 'twilio-video';
 import useParticipants from '../../hooks/useParticipants/useParticipants';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+import useAPIHook from './useAPIHook/useAPIHook'
+import useAPI from '../../hooks/useAPI/useAPI';
 
-interface State {
+export interface IState {
     state: any;
     joined: boolean;
     counter: number;
@@ -12,7 +14,7 @@ interface State {
 }
 
 interface IAPIContext {
-    state: State,
+    state: IState,
     joinHuddle: (huddle: string) => void,
     addHuddle: () => void,
     deleteUser: () => void,
@@ -21,62 +23,10 @@ interface IAPIContext {
 export const APIContext = createContext<IAPIContext>(null!)
 
 export function APIProvider({children: ReactNode}) {
-    const participants: RemoteParticipant[] = useParticipants();
     const { room } = useVideoContext();
     const localParticipant = room.localParticipant;
 
-    const [state, setState] = useState<State>({
-        state: {},
-        joined: false,
-        counter: 0,
-        huddle: -1,
-    });
-
-    if (!state.joined) {
-        console.log('Joining room...');
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-        };
-        var url = 'https://huddle-video.herokuapp.com/room/join?first=andy&last=jiang&username=da;sdf';
-        url += '&id=' + room.sid;
-        url += '&user_id=' + localParticipant.sid;
-
-        fetch(url, requestOptions)
-            .then(response => response.json())
-            .then(data => updateState(data));
-    }
-
-    function updateState(data: any) {
-        if (data.state_counter !== state.counter) {
-            // console.log(data);
-            var newState: {
-                [key: string]: any;
-            } = {};
-            participants.map(p => {
-                const huddleID: string = data.users[p.sid];
-                if (newState[huddleID] === undefined) {
-                    newState[huddleID] = [];
-                }
-                newState[huddleID].push(p);
-            });
-            const huddleID: string = data.huddle_id;
-            if (newState[huddleID] === undefined) {
-                newState[huddleID] = [];
-            }
-            newState[huddleID].push(localParticipant);
-
-            // console.log('new state...');
-            // console.log(newState);
-
-            setState({
-                state: newState,
-                joined: true,
-                counter: data.state_counter,
-                huddle: parseInt(data.huddle_id),
-            });
-        }
-    }
+    const [ state, updateState ] = useAPIHook()
 
     async function joinHuddle(huddle: string) {
         if (parseInt(huddle) != state.huddle) {
