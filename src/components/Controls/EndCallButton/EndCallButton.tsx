5a74bd6511f5aa46ae69a8b11031e87e2ea8549b
Zaiwen Gong <zgong@zaiwens-mbp.lan>
2020-08-02 21:03:26 -0400

    nothing, cleanup

--- src/components/Controls/EndCallButton/EndCallButton.tsx
+++ src/components/Controls/EndCallButton/EndCallButton.tsx
@@ -21,6 +21,7 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function EndCallButton() {
   const classes = useStyles();
   const { room } = useVideoContext();
+
   const { deleteUser } = useAPIContext()
 
   function leaveRoom() {
