e5ec88e09b37a629ff878a3c7c1cc3b6439f4659
Zaiwen Gong <zgong@zaiwens-mbp.lan>
2020-08-22 00:50:37 -0400

    reverted to prev commit, found issue with screenshare status

---
+++ src/components/ParticipantStrip/ParticipantStrip.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import HuddleVisualizer from '../HuddleVisualizer/HuddleVisualizer';
+import useParticipants from '../../hooks/useParticipants/useParticipants';
+import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+import useSelectedParticipant from '../VideoProvider/useSelectedParticipant/useSelectedParticipant';
+import { RemoteParticipant } from 'twilio-video';
+
+interface ParticipantStripProps {
+  zoomed: boolean;
+}
+
+// Without styled containers or scroll container
+export default function ParticipantStrip({ zoomed }: ParticipantStripProps) {
+  // const {
+  //   room: { localParticipant },
+  // } = useVideoContext();
+  const participants: RemoteParticipant[] = useParticipants();
+  // const [selectedParticipant, setSelectedParticipant] = useSelectedParticipant();
+  const { room } = useVideoContext();
+  // const [huddleID, setHuddleID] = useState('');
+
+  // console.log('participants:');
+  // console.log(participants);
+
+  return <HuddleVisualizer />;
+}
